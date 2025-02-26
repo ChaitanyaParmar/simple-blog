4044178c3d1703f7272958313df34e854bee9e98
Chaitanya <chaitanya1164m@gmail.com>
2025-02-26 17:21:21 +0530

    made minor changes

--- app/dashboard/(auth)/login/page.jsx
+++ app/dashboard/(auth)/login/page.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div className={styles.container}>Login</div>
+    <div>Login</div>
   )
 }
--- app/dashboard/(auth)/register/page.jsx
+++ app/dashboard/(auth)/register/page.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div className={styles.container}>Register</div>
+    <div>Register</div>
   )
 }
